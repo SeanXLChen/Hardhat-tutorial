9057c423e808a8ff2457af306b553af240f729d3
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-05-23 13:09:40 -0700

    modify config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_SEPOLIA_URL,
+      accounts: [process.env.SEAN_PRIVATE_KEY],
+    },
+    mainnet: {
+      url: process.env.ALCHEMY_MAINNET_URL,
+      accounts: [process.env.SEAN_PRIVATE_KEY],
+    },
+  }
 };
