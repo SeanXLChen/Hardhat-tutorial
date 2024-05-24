0bac54a7933adf08a5620dd4904c6da27240d56c
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-05-24 09:43:03 -0700

    Feat: enable SetOwner contract to receive eth

--- contracts/SetOwner.sol
+++ contracts/SetOwner.sol
@@ -2,10 +2,16 @@
 pragma solidity ^0.8.24;
 
 // this contract will set the owner to the address that deploys it
-contract Contract {
+contract SetOwner {
     address public owner;
 
     constructor() {
         owner = msg.sender; // msg.sender is the address that deploys the contract
+    }
+
+    // enable Function to receive any ether sent to this contract
+    receive() external payable {
+        // console.loa(msg.sender);
+        // console.log(msg.value);
     }
 }
