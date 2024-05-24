7e81c37808154b13f3b90814b9f19d2dae25fa1f
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-05-23 21:12:02 -0700

    Add new string state variable and modify function inside ModifyVariable Contract

--- contracts/ModifyVariable.sol
+++ contracts/ModifyVariable.sol
@@ -3,13 +3,19 @@ pragma solidity ^0.8.24;
 
 contract ModifyVariable {
   uint public x;
+  string public name;
 
-  constructor(uint _x) {
+  constructor(uint _x, string memory _name) {
     x = _x;
+    name = _name;
   }
 
   function modifyToLeet() public {
     x = 1337;
+  }
+
+  function modifyName(string memory _name) public {
+    name = _name;
   }
 
 }
