5ae7d0b61300fefd3243da1acfb4250557b64d3e
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-05-24 11:58:19 -0700

    Feat: add donate function to send contract eth balance out

--- contracts/SampleContract.sol
+++ contracts/SampleContract.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.24;
 // this contract will set the owner to the address that deploys it
 contract SampleContract {
     address public owner;
+    address payable public charityAddress;  // need to use payable keyword to send ether to this address
 
-    constructor() {
+    constructor(address payable _charityAddress) {
         owner = msg.sender; // msg.sender is the address that deploys the contract
+        charityAddress = _charityAddress;
     }
 
     // enable Function to receive any ether sent to this contract
@@ -24,5 +26,15 @@ contract SampleContract {
     function tipOwner() external payable {
         (bool success, ) = owner.call{ value: msg.value }("");
         require(success);
+    }
+
+    // no need to use the payable keyword here, since we are not sending any ether
+    function donate() external {
+        // Ensure only the owner can call this function
+        require(msg.sender == owner, "Only the owner can initiate the donation.");
+
+        // Send all the contract's balance to the charity address
+        (bool success, ) = charityAddress.call{ value: address(this).balance }("");
+        require(success, "Failed to send Ether");
     }
 }
