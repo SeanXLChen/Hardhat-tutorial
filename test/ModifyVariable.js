b1868933f2cbe29a24c5e03f8713d391720be26b
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-05-23 21:20:41 -0700

    Add more tests to Contract ModifyVariable

--- test/ModifyVariable.js
+++ test/ModifyVariable.js
@@ -8,12 +8,36 @@ describe("TestModifyVariable", function () {
       // this line creates an ethers ContractFactory abstraction: https://docs.ethers.org/v5/api/contract/contract-factory/
       const ModifyVariable = await ethers.getContractFactory("ModifyVariable");
   
-      const contract = await ModifyVariable.deploy(10);  // deploy the contract with an initial value of 10 for x
+      const contract = await ModifyVariable.deploy(10, 'Sean');  // deploy the contract with an initial value of 10 for x
   
       await contract.modifyToLeet(); // modify x from 10 to 1337 via this function!
 
       const newX = await contract.x(); // getter for state variable x
 
       expect(newX).to.equal(1337); // we expect the value of x to be 1337
+    });
+
+    // this test checks the state variable name is set to "Sean" 
+    it("should check the name is set to 'Sean'", async function () {
+      const ModifyVariable = await ethers.getContractFactory("ModifyVariable");
+  
+      const contract = await ModifyVariable.deploy(10, 'Sean');
+  
+      const storedName = await contract.name();
+  
+      expect(storedName).to.equal('Sean');
+    });
+
+    // this test checks name can be modify to "Sean's contract" when function modifyName is called
+    it("should change name to 'Sean's contract'", async function () {
+      const ModifyVariable = await ethers.getContractFactory("ModifyVariable");
+  
+      const contract = await ModifyVariable.deploy(10, 'Sean');
+  
+      await contract.modifyName("Sean's contract");
+  
+      const newName = await contract.name();
+  
+      expect(newName).to.equal("Sean's contract");
     });
   });
