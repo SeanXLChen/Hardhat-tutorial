33f2f50a0af12af7c8c4de26b472cbbe37fb3199
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-05-25 08:55:32 -0700

    Feat: add error handling for withdraw function

--- contracts/SampleRevert.sol
+++ contracts/SampleRevert.sol
@@ -2,7 +2,16 @@
 pragma solidity ^0.8.4;
 
 contract SampleRevert {
-    constructor() payable {  // payable constructor
-        require(msg.value >= 1e18, "Contract requires at least 1 ether to be deployed"); 
+    address payable public owner;
+
+    constructor() payable {
+        require(msg.value >= 1e18, "Please send at least 1 ETH!");
+        owner = payable(msg.sender);
     } 
+
+    function withdraw() public {
+        require(msg.sender == owner, "Only owner can withdraw fund.");
+        (bool s, ) = owner.call{value: address(this).balance}("");
+        require(s, "Failed to send Ether");
+    }
 }
