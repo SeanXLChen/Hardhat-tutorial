1742a034547c885634ccbda530ca9da094281774
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-05-26 10:01:10 -0700

    Fix: modify the format

--- contracts/04-mapping/contract.sol
+++ contracts/04-mapping/contract.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.24;
 
 contract Contract {
-    mapping (address => bool) public members; // hash table of addresses
+    mapping(address => bool) public members; // hash table of addresses
 
     function addMember(address addr) external {
-        members[addr] = true;  // set this address to true
+        members[addr] = true; // set this address to true
     }
 
     function removeMember(address addr) external {
         members[addr] = false;
     }
 
-    function isMember(address addr) external view returns(bool) {
+    function isMember(address addr) external view returns (bool) {
         return members[addr];
     }
 }
