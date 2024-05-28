f6d9d0d7e1f16b7ff2a416cc99872c934f8acc0e
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-05-28 07:53:23 -0700

    Feat: memory array size is fixed

--- contracts/08-arrays/storageArray.sol
+++ contracts/08-arrays/storageArray.sol
@@ -11,4 +11,21 @@ contract Contract {
             }
         }
     }
+
+    // memory array size is fixed
+    function filterEvenMemoryArry(uint[] memory arr) external pure returns (uint[] memory) {
+        uint[] memory evenArr = new uint[](arr.length);
+        uint count;
+        for (uint i; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                evenArr[count] = arr[i];
+                count++;
+            }
+        }
+        uint[] memory evenArrTrimmed = new uint[](count);
+        for (uint i; i < count; i++) {
+            evenArrTrimmed[i] = evenArr[i];
+        }
+        return evenArrTrimmed;
+    }
 }
