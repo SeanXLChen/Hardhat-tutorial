beece65cfecde28e93c55b7c542370e646d00c74
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-05-30 15:06:06 -0700

    Feat: implement abstract contract and override functions

--- contracts/15-Inheritance/SuperHeroes.sol
+++ contracts/15-Inheritance/SuperHeroes.sol
@@ -4,7 +4,13 @@ pragma solidity ^0.8.4;
 import "./Hero.sol";
 
 contract Mage is Hero(50) {
+    function attack(address enemy) public override {
+        Enemy(enemy).takeAttack(AttackTypes.Spell);
+    }
 }
 
 contract Warrior is Hero(200) {
+    function attack(address enemy) public override {
+        Enemy(enemy).takeAttack(AttackTypes.Brawl);
+    }
 }
