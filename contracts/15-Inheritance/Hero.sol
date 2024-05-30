e3a3181a2ae9640b4742bdab6e60be2515e8b0ff
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-05-30 15:13:03 -0700

    Feat: super to call parent contract's method

--- contracts/15-Inheritance/Hero.sol
+++ contracts/15-Inheritance/Hero.sol
@@ -5,16 +5,15 @@ interface Enemy {
 	function takeAttack(Hero.AttackTypes attackType) external;
 }
 
-abstract contract Hero {
+contract Hero {
 	uint public health;
+	uint public energy = 10;
 	constructor(uint _health) {
 		health = _health;
 	}
-	
-	function takeDamage(uint damage) external {
-		health -= damage;
-	}
-	
+
 	enum AttackTypes { Brawl, Spell }
-	function attack(address enemy) public virtual;
+	function attack(address) public virtual {
+		energy--;
+	}
 }
--- contracts/15-Inheritance/SuperHeroes.sol
+++ contracts/15-Inheritance/SuperHeroes.sol
@@ -6,11 +6,13 @@ import "./Hero.sol";
 contract Mage is Hero(50) {
     function attack(address enemy) public override {
         Enemy(enemy).takeAttack(AttackTypes.Spell);
+        super.attack(enemy);  // calls the parent contract's attack function
     }
 }
 
 contract Warrior is Hero(200) {
     function attack(address enemy) public override {
         Enemy(enemy).takeAttack(AttackTypes.Brawl);
+        super.attack(enemy);  // calls the parent contract's attack function
     }
 }
